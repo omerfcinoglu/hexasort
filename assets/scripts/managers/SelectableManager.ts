e1728776497f28d81e1ccfca601d75b92150c34e
Omer <62906173+l0rdyolo@users.noreply.github.com>
2024-11-22 19:01:09 +0300

    light update

--- assets/scripts/managers/SelectableManager.ts
+++ assets/scripts/managers/SelectableManager.ts
@@ -8,14 +8,14 @@ const { ccclass, property } = _decorator;
 export class SelectableManager extends Component {
 
     @property(Prefab)
-    selectableTilesPrefab: Prefab = null!; // SelectableTiles prefab'ına erişim
+    selectableTilesPrefab: Prefab = null!; 
     @property(Prefab)
-    tileClusterPrefab: Prefab = null!; // TileCluster prefab'ına erişim
+    tileClusterPrefab: Prefab = null!;
     @property(Node)
     public selectableArea: Node = null!;
 
     @property
-    tilesCount: number = 3; // Kaç tane SelectableTiles nesnesi oluşturulacak
+    tilesCount: number = 3; 
 
     private selectableTiles: SelectableTiles[] = [];
 
@@ -24,13 +24,13 @@ export class SelectableManager extends Component {
     }
 
     async createSelectableTiles() {
-        const startX = -(this.tilesCount - 1); // İlk nesnenin X pozisyonu
+        const startX = -(this.tilesCount - 1); 
         const startXOffset = 15
         for (let i = 0; i < this.tilesCount; i++) {
             const selectableTileNode = instantiate(this.selectableTilesPrefab);
             selectableTileNode.parent = this.selectableArea;
 
-            const position = new Vec3(startX + i * 2, 0, 0); // X ekseninde her nesneyi 3 birim aralıkla yerleştir
+            const position = new Vec3(startX + i * 2, 0, 0);
             selectableTileNode.setPosition(position);
             
             const selectableTile = selectableTileNode.getComponent(SelectableTiles);
@@ -48,8 +48,8 @@ export class SelectableManager extends Component {
     async addRandomClusters(selectableTile: SelectableTiles) {
         let clusterCount = Math.floor(Math.random() * 3) + 1;
         if(clusterCount === 1) clusterCount = 2;
-        const availableTypes = [1, 2, 3, 4, 5]; // Başlangıç tipi listesi
-        let lastClusterTileCount = 0;
+        const availableTypes = [1, 2, 3, 4, 5]; 
+        let lastClusterTileCount = 1;
     
         for (let i = 0; i < clusterCount; i++) {
             const randomIndex = Math.floor(Math.random() * availableTypes.length);
@@ -62,13 +62,11 @@ export class SelectableManager extends Component {
             if (cluster) {
                 cluster.initCluster(tileType, tileCount);
                 selectableTile.tileClusters.push(cluster);
-                cluster.node.setPosition(0, lastClusterTileCount * 0.1, 0);
+                cluster.node.setPosition(0, (lastClusterTileCount * 0.1), -0.01 * i);
             }
     
-            // console.log(`TileType ${tileType}\nTileCount ${tileCount}`);
-            // Kullanılan tip tekrar kullanılabilir hale gelmesi için availableTypes'a geri eklenir
+            console.log(`TileType ${tileType}\nTileCount ${tileCount}`);
             lastClusterTileCount += tileCount;
-            // console.log(`Selectable tile clusters: ${selectableTile.tileClusters.length}`);
         }
     }
     
