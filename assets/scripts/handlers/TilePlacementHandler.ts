96662a5e1fee1937f1214780039223d5113ca366
Omer <62906173+l0rdyolo@users.noreply.github.com>
2024-12-03 23:05:08 +0300

    Update TilePlacementHandler.ts

--- assets/scripts/handlers/TilePlacementHandler.ts
+++ assets/scripts/handlers/TilePlacementHandler.ts
@@ -14,20 +14,26 @@ export class TilePlacementHandler {
      * @param selectedTile The selected SelectableTiles instance to be placed.
      * @returns A promise resolving to true if placement is successful, otherwise false.
      */
-    async place(selectedTile: SelectableTiles,
-        selectableManager: SelectableManager): Promise<GroundTile> {
+    async place(selectedTile: SelectableTiles, selectableManager: SelectableManager): Promise<GroundTile> {
         const targetGround = selectedTile.attachedGround;
         if (!targetGround || !selectedTile) return null;
-            
-        // selectedTile.grounded();
 
+        this.addClustersToGround(selectedTile, targetGround);
+        this.finalizePlacement(selectedTile, selectableManager);
+
+        return targetGround;
+    }
+
+    private addClustersToGround(selectedTile: SelectableTiles, targetGround: GroundTile) {
         for (const cluster of selectedTile.tileClusters) {
             targetGround.addTileCluster(cluster);
         }
-    
+    }
+
+    private finalizePlacement(selectedTile: SelectableTiles, selectableManager: SelectableManager) {
+        const targetGround = selectedTile.attachedGround;
         targetGround.setActiveCollider(false);
         targetGround.isPlacedGround = true;
         selectableManager.remove(selectedTile);
-        return targetGround;
     }
 }
